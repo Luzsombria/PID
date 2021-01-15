76202d9370dc4092cfff4b28500921a9a9596d52
Javier <javicraft14@gmail.com>
2021-01-15 12:30:16 +0100

    Completada función RVIN para generar ruido impulsivo solamente

--- Código/RVIN.py
+++ Código/RVIN.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
+import random
 
 def generaRuido(imagen,porcentaje,nombre):
-    img = cv2.imread(imagen)
-    gauss = np.random.normal(0,1,img.size)
-    gauss = gauss.reshape(img.shape[0],img.shape[1],img.shape[2]).astype('uint8')
+    img = cv2.imread(imagen, cv2.IMREAD_GRAYSCALE)
+    rows, columns = img.shape
+    imgRuido = np.zeros( (rows, columns), dtype = np.uint8)
+    valorIN = random.randint(0,255)
+    
+    for x in range(0,rows):
+        for y in range(0,columns):
+            randomP = random.random()
+            if randomP <= porcentaje:
+                imgRuido[x,y] = valorIN
+            else:
+                imgRuido[x,y] = img[x,y]
 
-    img_gauss = cv2.add(img,gauss)
-
-    cv2.imwrite(imgRiudo,nombre)
+    cv2.imwrite(imgRuido,nombre)
